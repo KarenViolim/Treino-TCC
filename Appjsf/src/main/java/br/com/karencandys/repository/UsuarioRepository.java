475c94334190d648360782ebdb34bbb367718e1e
Karen Cristina Violim <36975539+KarenViolim@users.noreply.github.com>
2019-08-10 12:04:34 -0300

    Testes

---
+++ Appjsf/src/main/java/br/com/karencandys/repository/UsuarioRepository.java
@@ -0,0 +1,35 @@
+package br.com.karencandys.repository;
+
+import java.io.Serializable;
+
+import javax.persistence.Query;
+
+import br.com.karencandys.model.UsuarioModel;
+import br.com.karencandys.repository.entity.UsuarioEntity;
+import br.com.karencandys.uteis.Uteis;
+
+public class UsuarioRepository implements Serializable {
+	 
+	 
+	private static final long serialVersionUID = 1L;
+ 
+	public UsuarioEntity ValidaUsuario(UsuarioModel usuarioModel){
+ 
+		try {
+ 
+			//QUERY QUE VAI SER EXECUTADA (UsuarioEntity.findUser) 	
+			Query query = Uteis.JpaEntityManager().createNamedQuery("UsuarioEntity.findUser");
+ 
+			//PARÂMETROS DA QUERY
+			query.setParameter("usuario", usuarioModel.getUsuario());
+			query.setParameter("senha", usuarioModel.getSenha());
+ 
+			//RETORNA O USUÁRIO SE FOR LOCALIZADO
+			return (UsuarioEntity)query.getSingleResult();
+ 
+		} catch (Exception e) {
+ 
+			return null;
+		}
+ 	}
+}
